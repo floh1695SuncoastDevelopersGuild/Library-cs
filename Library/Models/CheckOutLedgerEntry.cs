4e108831b439c79b99582aea9154874d0670c436
Charles Bayley <cbayley95@gmail.com>
2018-04-19 21:27:10 -0400

    Added Reader model

--- Library/Models/CheckOutLedgerEntry.cs
+++ Library/Models/CheckOutLedgerEntry.cs
@@ -12,8 +12,8 @@ namespace Library.Models
         public int BookID { get; set; }
         public Book Book { get; set; }
 
-        //public int ReaderID { get; set; }
-        //public Reader Reader { get; set; }
+        public int ReaderID { get; set; }
+        public Reader Reader { get; set; }
 
         public DateTime Timestamp { get; set; }
     }
