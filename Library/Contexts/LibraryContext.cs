3bc6951928419d9803c0e969c62bfdfabdcbc185
Charles Bayley <cbayley95@gmail.com>
2018-04-19 20:37:43 -0400

    Added Genre to the DB

--- Library/Contexts/LibraryContext.cs
+++ Library/Contexts/LibraryContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -11,5 +12,7 @@ namespace Library.Contexts
         public LibraryContext()
             : base("name=Library")
         { }
+
+       public  DbSet<Genre> Genres { get; set; }
     }
 }
