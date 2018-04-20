58e7280ee108dd4ae3a52ec71ba7580f669d17fb
Charles Bayley <cbayley95@gmail.com>
2018-04-19 20:16:16 -0400

    Enabled the database for migrations and created a context
    Next: Add models

---
+++ Library/Contexts/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Library.Contexts
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext()
+            : base("name=Library")
+        { }
+    }
+}
