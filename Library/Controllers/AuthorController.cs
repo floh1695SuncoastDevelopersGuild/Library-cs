11e50c4d8b96a5836c3377b97e29155e59a0d223
Charles Bayley <cbayley95@gmail.com>
2018-04-20 09:16:36 -0400

    Added get author by id

---
+++ Library/Controllers/AuthorController.cs
@@ -0,0 +1,24 @@
+﻿using Library.Contexts;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Library.Controllers
+{
+    public class AuthorController : ApiController
+    {
+        public Author Get(int id)
+        {
+            Author author;
+            using (var db = new LibraryContext())
+            {
+                author = db.Authors.FirstOrDefault(a => a.ID == id);
+            }
+            return author;
+        }
+    }
+}
