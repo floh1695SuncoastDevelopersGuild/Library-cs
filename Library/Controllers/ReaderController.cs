18e32b085401cabf21c0eaaa30c912b7dad3b652
Charles Bayley <cbayley95@gmail.com>
2018-04-20 12:23:14 -0400

    Added ReaderController and related DbSet to Context

---
+++ Library/Controllers/ReaderController.cs
@@ -0,0 +1,33 @@
+﻿using Library.Contexts;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Library.Controllers
+{
+    public class ReaderController : ApiController
+    {
+        public Reader Get(int id)
+        {
+            Reader reader;
+            using (var db = new LibraryContext())
+            {
+                reader = db.Readers.FirstOrDefault(r => r.ID == id);
+            }
+            return reader;
+        }
+
+        public Reader Post(Reader reader)
+        {
+            using (var db = new LibraryContext())
+            {
+                db.Readers.Add(reader);
+            }
+            return reader;
+        }
+    }
+}
