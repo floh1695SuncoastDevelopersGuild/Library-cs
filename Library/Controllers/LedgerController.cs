c84d2b13f9780d2418ed1f0d48ae37137f639089
Charles Bayley <cbayley95@gmail.com>
2018-04-20 13:28:04 -0400

    Added LedgerController

---
+++ Library/Controllers/LedgerController.cs
@@ -0,0 +1,24 @@
+﻿using Library.Contexts;
+using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Library.Controllers
+{
+    public class LedgerController : ApiController
+    {
+        public IEnumerable<CheckOutLedgerEntry> Get()
+        {
+            IEnumerable<CheckOutLedgerEntry> ledger;
+            using (var db = new LibraryContext())
+            {
+                ledger = db.CheckOutLedger.ToList();
+            }
+            return ledger;
+        }
+    }
+}
