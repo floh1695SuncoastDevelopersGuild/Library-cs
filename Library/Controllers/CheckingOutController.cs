5f7124706640c3eace5c7b750a38f80f4e57d500
Charles Bayley <cbayley95@gmail.com>
2018-04-23 08:21:05 -0400

    Commit for submission

--- Library/Controllers/CheckingOutController.cs
+++ Library/Controllers/CheckingOutController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Library.Contexts;
+using Library.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,5 +12,36 @@ namespace Library.Controllers
 {
     public class CheckingOutController : ApiController
     {
+        // NOTE: Note complete
+        [Route("api/checking/out")]
+        public bool Post(int id)
+        {
+            bool rv;
+            using (var db = new LibraryContext())
+            {
+                var book = db.Books
+                    .Include(b => b.Ledger)
+                    .FirstOrDefault(b => b.ID == id);
+                if (book == null 
+                    || book.IsCheckedOut == true)
+                {
+                    rv = false;
+                }
+                else
+                {
+                    book.IsCheckedOut = false;
+                    book.Ledger.
+                        Add(new CheckOutLedgerEntry
+                        {
+                            BookID = id,
+                            Timestamp = DateTime.Now,
+                            ReaderID = 1 // TODO: Any user for now
+                        });
+                    rv = true;
+                }
+                db.SaveChanges();
+            }
+            return rv;
+        }
     }
 }
